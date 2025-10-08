474884d86d499021b0471d7e6e872204ebb7197d
Michael Wu <mw10013@gmail.com>
2025-10-08 17:38:58 -0400

    feat: implement Page and PageHeader components for the search functionality

---
+++ components/page-header.tsx
@@ -0,0 +1,24 @@
+import { twMerge } from "tailwind-merge";
+
+interface PageHeaderProps {
+  title: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default function PageHeader({
+  title,
+  className,
+  children,
+}: PageHeaderProps) {
+  return (
+    <div className={twMerge("mb-16 text-center", className)}>
+      <h1 className="font-heading mb-3 text-4xl/[1.1] font-bold tracking-tight text-foreground md:text-5xl/[1.1]">
+        {title}
+      </h1>
+      <p className="mx-auto max-w-3xl text-lg text-muted-foreground">
+        {children}
+      </p>
+    </div>
+  );
+}
