41ad64d6fdd6705a9d79228fb645f0c96d578490
Michael Wu <mw10013@gmail.com>
2025-10-12 18:51:43 -0400

    feat: update filter route to include items in loader data and enhance component rendering

--- app/routes/filter.tsx
+++ app/routes/filter.tsx
@@ -1,8 +1,11 @@
 import type { RegistryTag } from "@/registry/registry-tags";
 import type { LoaderFunctionArgs } from "react-router";
+import type { Route } from "./+types/filter";
 import PageHeader from "@/components/page-header";
 import { getAvailableTags, getComponents } from "@/lib/utils";
-import { useLoaderData } from "react-router";
+import { SelectEx } from "@/registry/components/oui-select-ex";
+import { ListBoxItem } from "@/registry/components/ui/oui-list-box";
+import { registryTags } from "@/registry/registry-tags";
 
 // export const metadata: Metadata = {
 //   title: "Filter Oui components",
@@ -10,6 +13,7 @@ import { useLoaderData } from "react-router";
 // }
 
 export function loader({ request }: LoaderFunctionArgs) {
+  const items = registryTags.map((tag) => ({ id: tag }));
   const url = new URL(request.url);
   const tagsParam = url.searchParams.get("tags");
   const tags = tagsParam
@@ -20,17 +24,26 @@ export function loader({ request }: LoaderFunctionArgs) {
     : [];
   const components = getComponents(tags as RegistryTag[]);
   const availableTags = getAvailableTags(tags as RegistryTag[]);
-  return { tags, availableTags, components };
+  return { tags, items, availableTags, components };
 }
 
-export default function RouteComponent() {
-  const data = useLoaderData<typeof loader>();
+export default function RouteComponent({
+  loaderData: { items, ...loaderData },
+}: Route.ComponentProps) {
   return (
     <>
       <PageHeader title="Filter" className="mb-10">
         Use this page to filter components by tags.
       </PageHeader>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <SelectEx
+        aria-label="Select tags"
+        items={items}
+        selectionMode="multiple"
+        placeholder="Select tags"
+      >
+        {(item) => <ListBoxItem>{item.id}</ListBoxItem>}
+      </SelectEx>
+      <pre>{JSON.stringify(loaderData, null, 2)}</pre>
     </>
   );
 }
