44c85c404376a3f7dc105e5a9f23e8df34f92631
Michael Wu <mw10013@gmail.com>
2025-10-13 22:40:05 -0400

    refactor: update GitHub link in header component and replace icon with simple-icons

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
+import { buttonClassName } from "@/registry/components/ui/oui-button";
 import { Link } from "@/registry/components/ui/oui-link";
-import { Github } from "lucide-react";
+import { siGithub } from "simple-icons";
 
 export default function Header() {
   return (
@@ -9,15 +10,17 @@ export default function Header() {
           Oui
         </Link>
         <div className="flex items-center gap-1">
-          <a
-            className="inline-flex size-9 items-center justify-center rounded text-muted-foreground outline-none hover:text-foreground/80 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50"
-            href="https://github.com/origin-space/originui"
+          <Link
+            href="https://github.com/mw10013/oui"
             target="_blank"
             rel="noopener noreferrer"
+            className={buttonClassName({ variant: "ghost" })}
           >
             <span className="sr-only">GitHub</span>
-            <Github size={20} />
-          </a>
+            <svg viewBox="0 0 24 24" width={20} height={20} fill="currentColor">
+              <path d={siGithub.path} />
+            </svg>
+          </Link>
           {/* <ThemeToggle /> */}
         </div>
       </div>
