277930fc0aac8e4045f0191fc8042194b5d7c6fc
Michael Wu <mw10013@gmail.com>
2025-10-16 23:25:50 -0400

    feat: update oui-header-demo component to include menu and button functionality in registry

--- registry/components/oui-header-demo.tsx
+++ registry/components/oui-header-demo.tsx
@@ -1,5 +1,20 @@
+import { MenuEx } from "@/registry/components/oui-menu-ex";
+import { Button } from "@/registry/components/ui/oui-button";
 import { Header } from "@/registry/components/ui/oui-header";
+import { MenuItem } from "@/registry/components/ui/oui-menu";
+import * as Rac from "react-aria-components";
 
 export default function Component() {
-  return <Header>Header</Header>;
+  return (
+    <MenuEx
+      triggerElement={<Button variant="outline">Header</Button>}
+      className="w-56"
+    >
+      <Rac.MenuSection>
+        <Header variant="menu">Header</Header>
+        <MenuItem id="profile">Profile</MenuItem>
+        <MenuItem id="settings">Settings</MenuItem>
+      </Rac.MenuSection>
+    </MenuEx>
+  );
 }
