db52694414c581e97c64497b738e01e439af1a67
Michael Wu <mw10013@gmail.com>
2025-10-19 22:26:09 -0400

    Revert: bring back OpenInV0 component

--- components/component-details.tsx
+++ components/component-details.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import ComponentCli from "@/components/cli-commands";
 import CodeBlock, { highlight } from "@/components/code-block";
 import CopyButton from "@/components/copy-button";
-import CopyRegistry from "@/components/copy-registry";
+import OpenInV0 from "@/components/open-in-v0";
 import { DialogEx } from "@/registry/components/oui-dialog-ex";
 import { TooltipEx } from "@/registry/components/oui-tooltip-ex";
 import { Button } from "@/registry/components/ui/oui-button";
@@ -65,8 +65,8 @@ export default function ComponentDetails({
 
   return (
     <div className="absolute top-2 right-2 flex gap-1 peer-data-comp-loading:hidden">
-      <CopyRegistry
-        url={`https://oui.mw10013.workers.dev/r/${component.name}.json`}
+      <OpenInV0
+        componentSource={`https://oui.mw10013.workers.dev/r/${component.name}.json`}
       />
       <DialogEx
         className="sm:max-w-[600px]"
