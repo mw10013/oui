d8f8a7147c1c0135e394434cf2d716242681c112
Michael Wu <mw10013@gmail.com>
2025-10-23 10:12:30 -0400

    feat: implement AppSidebar and RouteComponent for sidebar navigation

---
+++ app/routes/play.sidebar.tsx
@@ -0,0 +1,43 @@
+import * as Oui from "@/registry/default/ui/oui-index";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarProvider,
+} from "@/registry/default/ui/sidebar";
+
+export function AppSidebar() {
+  const items = [
+    {
+      id: "SaaS",
+      href: "/",
+    },
+    {
+      id: "Admin",
+      href: "/admin",
+    },
+    {
+      id: "Users",
+      href: "/admin/users",
+    },
+  ];
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <Oui.SidebarExTree aria-label="Admin Navigation" items={items} />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+export default function RouteComponent() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <Oui.SidebarExTrigger className="m-2" />
+        <div className="flex flex-col gap-2 px-4">Sidebar Playground</div>
+      </main>
+    </SidebarProvider>
+  );
+}
