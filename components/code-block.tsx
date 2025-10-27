1deba1c941a0ef19723ed77d022ae67de2db763f
Michael Wu <mw10013@gmail.com>
2025-10-27 19:37:46 -0400

    fix: correct CSS max-height unit from vh to svh in CodeBlock component

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -62,7 +62,7 @@ export default function CodeBlock({
   }, [code, lang, preHighlighted]);
 
   return content ? (
-    <div className="[&_code]:font-mono [&_code]:text-[13px] [&_pre]:max-h-[calc(70vh)] [&_pre]:overflow-auto [&_pre]:rounded-md [&_pre]:bg-zinc-950! [&_pre]:p-4 [&_pre]:leading-snug dark:[&_pre]:bg-zinc-900!">
+    <div className="[&_code]:font-mono [&_code]:text-[13px] [&_pre]:max-h-[calc(70svh)] [&_pre]:overflow-auto [&_pre]:rounded-md [&_pre]:bg-zinc-950! [&_pre]:p-4 [&_pre]:leading-snug dark:[&_pre]:bg-zinc-900!">
       {content}
     </div>
   ) : (
