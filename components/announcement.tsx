19f554a4a94ffa6a0949bc96f25e42b90b76d0d9
Michael Wu <mw10013@gmail.com>
2025-11-14 15:51:09 -0500

    feat: refactor Announcement component to use react-aria-components for improved accessibility

--- components/announcement.tsx
+++ components/announcement.tsx
@@ -1,18 +1,18 @@
 import { Badge } from "@/registry/default/ui/badge";
-import * as Oui from "@/registry/default/ui/oui-index";
 import { ArrowRightIcon } from "lucide-react";
+import * as Rac from "react-aria-components";
 
 export function Announcement() {
   return (
     <Badge asChild variant="secondary" className="rounded-full">
-      <Oui.Link
+      <Rac.Link
         href="https://github.com/mw10013/cloudflare-react-router-saas"
         target="_blank"
         rel="noopener noreferrer"
       >
         <span className="flex size-2 rounded-full bg-blue-500" title="New" />
         Example: Open-source saas template using Oui <ArrowRightIcon />
-      </Oui.Link>
+      </Rac.Link>
     </Badge>
   );
 }
