0ef9e4ecff5c7578f486567fdc4c3ea18de85f63
Michael Wu <mw10013@gmail.com>
2025-11-13 23:11:36 -0500

    feat: enhance Announcement component link with target and rel attributes for improved security and usability

--- components/announcement.tsx
+++ components/announcement.tsx
@@ -5,7 +5,11 @@ import { ArrowRightIcon } from "lucide-react";
 export function Announcement() {
   return (
     <Badge asChild variant="secondary" className="rounded-full">
-      <Oui.Link href="https://github.com/mw10013/cloudflare-react-router-saas">
+      <Oui.Link
+        href="https://github.com/mw10013/cloudflare-react-router-saas"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <span className="flex size-2 rounded-full bg-blue-500" title="New" />
         Example: Open-source saas template using Oui <ArrowRightIcon />
       </Oui.Link>
