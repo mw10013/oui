9a56c7eb293501d4683de652f15fe42cbabd188b
Michael Wu <mw10013@gmail.com>
2025-11-28 16:59:30 -0500

    feat: update oui-checkbox-group-demo-invalid component to include validation and improve item selection logic

--- registry/default/components/oui-checkbox-group-demo-invalid.tsx
+++ registry/default/components/oui-checkbox-group-demo-invalid.tsx
@@ -8,31 +8,34 @@ import {
   FieldGroup,
   FieldLabel,
 } from "@/registry/default/ui/oui-field";
+import * as Rac from "react-aria-components";
 
 export default function Component() {
   return (
     <div className="w-full max-w-md">
-      <FieldGroup>
-        <CheckboxGroup defaultValue={["hard-disks"]}>
-          <FieldLabel>Show these items on the desktop</FieldLabel>
-          <FieldDescription>
-            Select the items you want to show on the desktop.
-          </FieldDescription>
-          <Checkbox value="hard-disks">
-            Hard disks
-          </Checkbox>
-          <Checkbox value="external-disks">
-            External disks
-          </Checkbox>
-          <Checkbox value="cds-dvds" isDisabled>
-            CDs, DVDs, and iPods
-          </Checkbox>
-          <Checkbox value="connected-servers">
-            Connected servers
-          </Checkbox>
-          <FieldError />
-        </CheckboxGroup>
-      </FieldGroup>
+      <Rac.Form
+        validationErrors={{
+          "desktop-items":
+            "Hard disks and external disks cannot be selected together.",
+        }}
+      >
+        <FieldGroup>
+          <CheckboxGroup
+            name="desktop-items"
+            defaultValue={["hard-disks", "external-disks"]}
+          >
+            <FieldLabel>Show these items on the desktop</FieldLabel>
+            <FieldDescription>
+              Select the items you want to show on the desktop.
+            </FieldDescription>
+            <Checkbox value="hard-disks">Hard disks</Checkbox>
+            <Checkbox value="external-disks">External disks</Checkbox>
+            <Checkbox value="cds-dvds">CDs, DVDs, and iPods</Checkbox>
+            <Checkbox value="connected-servers">Connected servers</Checkbox>
+            <FieldError />
+          </CheckboxGroup>
+        </FieldGroup>
+      </Rac.Form>
     </div>
   );
 }
